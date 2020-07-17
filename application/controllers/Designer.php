54dac847aa0d7878719991d35e99c1da1f666f8e
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2020-07-17 17:28:07 +0700

    Initial commit

---
+++ application/controllers/Designer.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Designer extends CI_Controller
+{
+	public function index()
+	{
+		echo "designer";
+	}
+}
---
+++ application/controllers/Paket.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Paket extends CI_Controller
+{
+	public function index()
+	{
+		echo "paket";
+	}
+}
