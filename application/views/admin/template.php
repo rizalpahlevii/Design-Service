284c989b356c24434afafdb7faecbb5185bf7fcd
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2020-07-20 09:45:38 +0700

    Remove profile button

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -77,29 +77,11 @@
 
 									<p>
 										<?= $this->session->userdata('username') ?> - Admin
-										<small>Member since Nov. 2012</small>
+
 									</p>
-								</li>
-								<!-- Menu Body -->
-								<li class="user-body">
-									<div class="row">
-										<div class="col-xs-4 text-center">
-											<a href="#">Followers</a>
-										</div>
-										<div class="col-xs-4 text-center">
-											<a href="#">Sales</a>
-										</div>
-										<div class="col-xs-4 text-center">
-											<a href="#">Friends</a>
-										</div>
-									</div>
-									<!-- /.row -->
 								</li>
 								<!-- Menu Footer-->
 								<li class="user-footer">
-									<div class="pull-left">
-										<a href="#" class="btn btn-default btn-flat">Profile</a>
-									</div>
 									<div class="pull-right">
 										<a href="<?= site_url('backoffice/auth/logout') ?>" class="btn btn-default btn-flat">Sign out</a>
 									</div>
