1a10032f0f6cbd11d3303b1e6a174eab38c66a0c
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2020-07-20 16:14:11 +0700

    Impove

--- application/controllers/backoffice/Dashboard.php
+++ application/controllers/backoffice/Dashboard.php
@@ -15,6 +15,9 @@ class Dashboard extends CI_Controller
 	}
 	public function index()
 	{
+		$data['pesanan_hari_ini'] = $this->db->select('*')->where('tanggal_pemesanan', date('Y-m-d'))->from('pemesanan')->get()->num_rows();
+		$data['pendapatan_hari_ini'] = $this->db->select('SUM(total_bayar) as ttl')->where('tanggal_pemesanan', date('Y-m-d'))->from('pemesanan')->get()->row();
+		$data['jumlah_pesanan'] = $this->db->select('SUM(jumlah_pemesanan) as jml')->where('tanggal_pemesanan', date('Y-m-d'))->from('pemesanan')->get()->row();
 		$data['page'] = 'Dashboard';
 		$data['kategori'] = $this->db->get('kategori')->num_rows();
 		$data['paket'] = $this->db->get('paket')->num_rows();
--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -48,12 +48,43 @@
 		<!-- small box -->
 		<div class="small-box bg-red">
 			<div class="inner">
-				<h3>65</h3>
+				<h3><?= $pesanan_hari_ini ?></h3>
 
-				<p>Unique Visitors</p>
+				<p>Order Hari ini</p>
 			</div>
 			<div class="icon">
 				<i class="ion ion-pie-graph"></i>
+			</div>
+			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+	<!-- ./col -->
+</div>
+<div class="row">
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-aqua">
+			<div class="inner">
+				<h3><?= $pendapatan_hari_ini->ttl ?></h3>
+
+				<p>Pendapatan Hari Ini</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
+			</div>
+			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-yellow">
+			<div class="inner">
+				<h3><?= $jumlah_pesanan->jml ?></h3>
+
+				<p>Jumlah Pesanan Hari Ini</p>
+			</div>
+			<div class="icon">
+				<i class="ion ion-bag"></i>
 			</div>
 			<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 		</div>
